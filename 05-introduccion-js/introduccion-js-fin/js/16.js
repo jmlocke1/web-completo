ba1ab0c27156cb9f8bb75a7b5f3578bab551a3a3
josemi <worldcode.info@gmail.com>
2021-08-13 09:05:37 +0200

    Comienzo de la introducción a Javascript

---
+++ 05-introduccion-js/introduccion-js-fin/js/16.js
@@ -0,0 +1,16 @@
+
+// Declaración de Función
+sumar();
+function sumar() {
+    console.log(10 + 10);
+}
+
+
+
+// Expresión de la función
+sumar2();
+const sumar2 = function() {
+    console.log( 3 + 3);
+}
+
+
