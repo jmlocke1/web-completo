4825ebb5bebb40b455e046696feed06ea9f3ed1e
Josemi Xeon <worldcode.info@gmail.com>
2021-08-15 22:20:57 +0200

    Números en Javascript

---
+++ 05-introduccion-js/introduccion-js-inicio/js/04.js
@@ -0,0 +1,12 @@
+const tweet = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo';
+const producto2 = 'Monitor HD"';
+console.log(tweet, "tiene", tweet.length, "caracteres");
+console.log(producto2);
+
+// IndexOf (retorna posición)
+console.log(tweet.indexOf('JavaScript'));
+console.log(producto2.indexOf('Tablet'));
+
+// Includes (retorna true o false)
+console.log(tweet.includes('JavaScript'));
+console.log(producto2.includes('Tablet'));
---
+++ 05-introduccion-js/introduccion-js-inicio/js/05.js
@@ -0,0 +1,14 @@
+// Números
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 7;
+const numero4 = 3;
+const numero5 = -5;
+
+console.log(numero1 + numero2);
+console.log(numero2);
+console.log(numero3);
+//Módulo
+const operacionModulo = `${numero3} / ${numero4} = ${Math.floor(numero3 / numero4)}, y el resto es: ${numero3 % numero4}`;
+console.log(operacionModulo);
