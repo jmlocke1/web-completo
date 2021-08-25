f7c6f95c6f22fc3453a220dd485f48b71ce1b79a
josemi <worldcode.info@gmail.com>
2021-08-25 23:43:43 +0200

    Bucles varios

---
+++ 05-introduccion-js/introduccion-js-inicio/js/25.js
@@ -0,0 +1,28 @@
+const carrito = [
+	{ nombre: 'Monitor 20 pulgadas', precio: 500 },
+	{ nombre: 'Televisión 50 pulgadas', precio: 700 },
+	{ nombre: 'Tablet', precio: 300 },
+	{ nombre: 'Audífonos', precio: 200 },
+	{ nombre: 'Teclado', precio: 50 },
+	{ nombre: 'Celular', precio: 500 },
+	{ nombre: 'Altavoces', precio: 300 },
+	{ nombre: 'Laptop', precio: 800 }
+];
+
+// ForEach
+// carrito.forEach(function(producto){
+//     console.log(`Producto: ${producto.nombre}, precio: ${producto.precio}`);
+// });
+
+// Con Arrow Function
+// const array1 = carrito.forEach(producto => console.log(`Producto: ${producto.nombre}, precio: ${producto.precio}`));
+
+// Map
+const array2 = carrito.map(producto => producto.nombre);
+const array1 = carrito.forEach(producto => producto.nombre);
+// La diferencia entre forEach y map es que map devuelve 
+// un array, mientras que forEach no devuelve nada
+// Esto devolverá undefined
+console.log(array1);
+// Esto devolverá un array con los nombres de los productos
+console.log(array2);
