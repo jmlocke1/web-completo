375c8b7a76cfcd255e6c6ef95841b4cc042aeca2
josemi <worldcode.info@gmail.com>
2021-08-28 04:30:44 +0200

    Comenzando con DOM JavaScript

---
+++ 06-dom-js/JavaScript_DOM_Eventos_inicio/js/scripts.js
@@ -0,0 +1,10 @@
+// querySelector
+const heading = document.querySelector('.header__texto h2');
+heading.textContent = "Nuevo Heading";
+console.log(heading);
+
+// querySelectorAll
+const enlaces = document.querySelectorAll('.navegacion a');
+enlaces[0].textContent = 'Nuevo texto para enlace';
+
+// getElementById
