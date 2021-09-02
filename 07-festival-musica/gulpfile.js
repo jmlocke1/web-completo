90eb1a9cccc01f70584c8ebeae60a1f3c76d1c4d
Josemi Xeon <worldcode.info@gmail.com>
2021-09-03 01:26:00 +0200

    Primeros pasos con Gulp y SASS

--- 07-festival-musica/gulpfile.js
+++ 07-festival-musica/gulpfile.js
@@ -1,10 +1,30 @@
-const { src, dest } = require('gulp');
+const { src, dest, series, parallel, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
+// const rename = require('gulp-rename');
 function compilarSASS(){
-	return src("./src/scss/app.scss")
+	return src("./src/scss/*.scss")
 		.pipe( sass() )
 		.pipe( dest("./build/css") );
 }
 
-exports.compilarSASS = compilarSASS
+function minificarCSS(){
+	return src('./src/scss/*.scss')
+		.pipe( sass({
+			outputStyle: 'compressed'
+		}) )
+		.pipe( dest("./build/min.css"))
+}
+
+function watchArchivos(){
+	watch( "./src/scss/*.scss", series(compilarSASS, minificarCSS));
+}
+
+exports.compilarSASS = compilarSASS;
+exports.minificarCSS = minificarCSS;
+exports.watchArchivos = watchArchivos;
+// Ejecutando gulp tareas se ejecutan todas las tareas en serie
+// exports.tareas = series(compilarSASS, compilarJavaScript );
+// Ejecutando simplemente gulp se ejecutan todas las funciones
+// La función parallel lanza todas las tareas en paralelo
+// exports.default = parallel(compilarSASS, compilarJavaScript, minificarCSS );
+exports.default = series(compilarSASS, minificarCSS );
