b1ea79181f4dadb4f8a4a4f88ccc919aff0a8fc8
Josemi Xeon <worldcode.info@gmail.com>
2021-09-30 21:45:04 +0200

    Creando los servicios

--- 08-app-salon/08-app-salon/gulpfile.js
+++ 08-app-salon/08-app-salon/gulpfile.js
@@ -35,7 +35,6 @@ function javascript() {
       .pipe(sourcemaps.init())
       .pipe(concat('bundle.js')) // final output file name
       .pipe(terser())
-      //.pipe(sourcemaps.write('.'))
       .pipe(rename({ suffix: '.min' }))
       .pipe(sourcemaps.write('.'))
       .pipe(dest('./build/js'))
--- 08-app-salon/08-app-salon/src/js/app.js
+++ 08-app-salon/08-app-salon/src/js/app.js
@@ -12,15 +12,47 @@ async function mostrarServicios() {
 		const db = await resultado.json();
 
 		const {servicios} = db;
-
+		//const listadoServicios = document.querySelector('#servicios');
+		let servicioDiv = '';
 		// Generar el HTML
 		servicios.forEach(servicio => {
 			const { id, nombre, precio } = servicio;
 
 			// DOM Scripting
+			/*
+			// Generar nombre de servicio
+			const nombreServicio = document.createElement('P');
+			nombreServicio.textContent = nombre;
+			nombreServicio.classList.add('nombre-servicio');
+			// Generar precio
+			const precioServicio = document.createElement('P');
+			precioServicio.textContent = `$ ${precio}`;
+			precioServicio.classList.add('precio-servicio');
+
+			// Generar div contenedor de servicio
+			const servicioDiv = document.createElement('DIV');
+			servicioDiv.classList.add('servicio');
+
+			// Inyectar precio y nombre al div de servicio
+			servicioDiv.appendChild(nombreServicio);
+			servicioDiv.appendChild(precioServicio);
+			*/
+			
+			// Añadir el servicio al listado de servicios
+			// listadoServicios.appendChild(servicioDiv);
+
+			// Solución con innerHTML
+			servicioDiv += `
+					<div class="servicio" data-id-servicio="${id}">
+						<p class="nombre-servicio">${nombre}</p>
+						<p class="precio-servicio">$ ${precio}</p>
+					</div>`;
 		});
+		// Insertamos el HTML
+		document.querySelector('#servicios').innerHTML = servicioDiv;
 	} catch (error) {
 		console.log('Fichero no encontrado: ');
 		console.log(error);
 	}
+	
 }
