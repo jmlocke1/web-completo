3a3f67f996c2ddb1a398dbe610f3823c6103fde1
Josemi Xeon <worldcode.info@gmail.com>
2021-10-10 21:47:47 +0200

    Creando el header

--- 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
+++ 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber');
 
 function css( done ) {
 	// Identificar el archivo css a compilar
 	src('src/scss/**/*.scss')
+		.pipe( plumber() )
 		.pipe( sass() )  // Compilarlo
 		.pipe( dest('./build/css') )  // Almacenarla en el disco duro
 	done();
