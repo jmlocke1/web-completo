5786d19e27239fa0ce1eb21dabb92913e9fa7143
Josemi Xeon <worldcode.info@gmail.com>
2021-10-11 22:46:16 +0200

    Usando Gulp para crear imágenes Webp y aligerar imágenes jpg y png

--- 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
+++ 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
@@ -1,6 +1,13 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imágenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css( done ) {
 	// Identificar el archivo css a compilar
@@ -11,10 +18,35 @@ function css( done ) {
 	done();
 }
 
+function imagenes( done ) {
+	const opciones = {
+		optimizationLevel: 3
+	};
+	src('src/img/**/*.{png,jpg}')
+		.pipe( cache( imagemin(opciones) ) )
+		.pipe( dest('build/img'))
+
+	done();
+}
+
+function versionWebp( done ) {
+	const opciones = {
+		quality: 50
+	};
+	src('src/img/**/*.{png,jpg}')
+		.pipe( webp(opciones) )
+		.pipe( dest('build/img') );
+
+	done();
+}
+
 function dev( done ) {
 	watch('src/scss/**/*.scss', css);
 	done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.minImages = parallel(imagenes, versionWebp)
+exports.dev = parallel( dev );
