99d999d91b9f1459643abc7b5fcc7c3af28c815b
josemi <worldcode.info@gmail.com>
2021-10-21 23:36:52 +0200

    Añadiendo plumber

--- 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
+++ 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
@@ -1,65 +1,18 @@
-const { src, dest, watch, parallel, series } = require('gulp');
-
-// CSS
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
-// Imágenes
-const cache = require('gulp-cache');
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
-
 function css( done ) {
-	// Identificar el archivo css a compilar
-	src('src/scss/**/*.scss')
-		.pipe( plumber() )
-		.pipe( sass() )  // Compilarlo
-		.pipe( dest('./build/css') )  // Almacenarla en el disco duro
-	done();
-}
-
-function imagenes( done ) {
-	const opciones = {
-		optimizationLevel: 3
-	};
-	src('src/img/**/*.{png,jpg}')
-		.pipe( cache( imagemin(opciones) ) )
-		.pipe( dest('build/img'))
-
-	done();
-}
-
-function versionWebp( done ) {
-	const opciones = {
-		quality: 50
-	};
-	src('src/img/**/*.{png,jpg}')
-		.pipe( webp(opciones) )
-		.pipe( dest('build/img') );
-
-	done();
-}
-
-function versionAvif( done ) {
-	const opciones = {
-		quality: 50
-	};
-	src('src/img/**/*.{png,jpg}')
-		.pipe( avif(opciones) )
-		.pipe( dest('build/img') );
-
-	done();
+    src('src/scss/**/*.scss')   // Identificar el archivo .SCSS a compilar
+        .pipe( sass() )         // Compilarlo
+        .pipe( plumber() )
+        .pipe( dest('build/css') )
+    done();
 }
 
 function dev( done ) {
-	watch('src/scss/**/*.scss', css);
-	done();
+    watch('src/scss/**/*.scss', css);
 }
 
 exports.css = css;
-exports.imagenes = imagenes;
-exports.versionWebp = versionWebp;
-exports.versionAvif = versionAvif;
-exports.minImages = parallel(imagenes, versionWebp, versionAvif);
-exports.dev = parallel( dev );
+exports.dev = dev;
