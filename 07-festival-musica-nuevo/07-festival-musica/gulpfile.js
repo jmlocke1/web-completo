e891b3c4597fec96f80b37ce091ce47f2eee93f7
josemi <worldcode.info@gmail.com>
2021-10-26 22:49:44 +0200

    Añadidos varios plugins gulp en linux. Fallo en gulp-avif, en cuanto se importa el plugin, falla todo

--- 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
+++ 07-festival-musica-nuevo/07-festival-musica/gulpfile.js
@@ -1,6 +1,14 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+// CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+// Imágenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+//const avif = require('gulp-avif');
 
 function css( done ) {
     src('src/scss/**/*.scss')   // Identificar el archivo .SCSS a compilar
@@ -10,9 +18,46 @@ function css( done ) {
     done();
 }
 
+function imagenes( done ) {
+    const opciones = {
+        optimizationLevel: 3
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img') )
+    
+    done();
+}
+
+function versionWebp( done ){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') );
+
+    done();
+}
+
+function versionAvif( done ){
+    const opciones = {
+        quality: 50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') );
+
+    done();
+}
+
 function dev( done ) {
     watch('src/scss/**/*.scss', css);
 }
 
 exports.css = css;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.minImages = parallel(imagenes, versionWebp);
 exports.dev = dev;
