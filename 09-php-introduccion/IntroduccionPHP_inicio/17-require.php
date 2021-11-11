3dc0c8852451e4d1e5e69ddeaa93c800af784293
josemi <worldcode.info@gmail.com>
2021-11-11 13:54:31 +0100

    Creada estructura de Bienes Raíces. Hay que revisar el Package.json para actualizarlo

--- 09-php-introduccion/IntroduccionPHP_inicio/17-require.php
+++ 09-php-introduccion/IntroduccionPHP_inicio/17-require.php
@@ -5,7 +5,7 @@ include 'includes/header.php';
 require 'funciones.php';
 iniciarApp();
 
-
+echo "<p>Última modificación del archivo: " . date("F d Y H:i:s.", getlastmod())."</p>";
 
 
 include 'includes/footer.php';
