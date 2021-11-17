f5a1372a6b907a08504bab0d319488aec567e30a
josemi <worldcode.info@gmail.com>
2021-11-17 22:36:10 +0100

    Arreglada la visibilidad del menú de navegación cuando cambia a teléfono

--- 11-Bienes-Raices/bienesraices_inicio/src/js/app.js
+++ 11-Bienes-Raices/bienesraices_inicio/src/js/app.js
@@ -1,3 +1,5 @@
+let oldSize = window.innerWidth;
+
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
 });
@@ -6,21 +8,46 @@ function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
 
     mobileMenu.addEventListener('click', navegacionResponsive);
-    // window.addEventListener('resize', cambioTamano);
+    window.addEventListener('resize', cambioTamano);
 }
 
 function cambioTamano() {
     const navegacion = document.querySelector('.navegacion');
-    if(window.innerWidth > 768) {
-        navegacion.classList.contains('mostrar') ? navegacion.classList.remove('mostrar') : '';
+    let newSize = window.innerWidth;
+    if( newSize <= 768 && newSize < oldSize && !navegacion.classList.contains('mostrar')) {
+        temporaryClass(navegacion, 'visibilidadTemporal', 500);
     } 
+    oldSize = newSize;
 }
 
 function navegacionResponsive() {
     const navegacion = document.querySelector('.navegacion');
     if(navegacion.classList.contains('mostrar')) {
         navegacion.classList.remove('mostrar');
+        temporaryClass(navegacion, 'alturaTemporal', 500);
     } else {
         navegacion.classList.add('mostrar');
     }
+}
+
+// function heightTransition( elemento ) {
+//     elemento.classList.add('alturaTemporal');
+//     setTimeout(() => {
+//         elemento.classList.remove('alturaTemporal');
+//     }, 500);
+// }
+
+/**
+ * Función que añade a un elemento una clase temporalmente durante
+ * el tiempo pasado por parámetro
+ * 
+ * @param {*} element       Nodo del DOM al que añadir la clase
+ * @param {*} className     Nombre de la clase a añadir
+ * @param {*} time          Tiempo que tiene que estar añadida la clase
+ */
+function temporaryClass ( element, className, time ) {
+    element.classList.add(className);
+    setTimeout(() => {
+        element.classList.remove(className);
+    }, time);
 }
