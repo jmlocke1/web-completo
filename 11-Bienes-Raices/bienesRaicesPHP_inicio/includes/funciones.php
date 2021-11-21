2a480bed6c19569f39fa0abe1bc756edc2c07b0e
josemi <worldcode.info@gmail.com>
2021-11-22 00:43:24 +0100

    Función para añadir un título distinto a cada página

--- 11-Bienes-Raices/bienesRaicesPHP_inicio/includes/funciones.php
+++ 11-Bienes-Raices/bienesRaicesPHP_inicio/includes/funciones.php
@@ -3,3 +3,7 @@ require 'app.php';
 function incluirTemplate( string $nombre, string $inicio = '' ) {
     include TEMPLATES_URL."/${nombre}.php";
 }
+
+function getTitle() {
+    return ucfirst(basename($_SERVER["SCRIPT_FILENAME"], '.php'));
+}
