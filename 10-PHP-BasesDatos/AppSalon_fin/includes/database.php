ac36d44db8a2e408bf07dc34ef6a435cc8846c0e
josemi <worldcode.info@gmail.com>
2021-11-25 13:52:21 +0100

    Pequeños cambios en Appsalon base datos

--- 10-PHP-BasesDatos/AppSalon_fin/includes/database.php
+++ 10-PHP-BasesDatos/AppSalon_fin/includes/database.php
@@ -3,6 +3,7 @@ $db = mysqli_connect('localhost', 'usprueba', 'usprueba', 'appsalon');
 
 if(!$db){
     echo "Error en la conexión";
+    exit();
 } else {
-    //echo "Conexión correcta";
+    // echo "Conexión correcta";
 }
