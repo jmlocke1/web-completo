78aff7efe173da9cb08ee17d07b91fcf3fc2c71d
josemi <worldcode.info@gmail.com>
2021-11-30 14:25:57 +0100

    Listando las propiedades

--- 11-Bienes-Raices/bienesRaicesPHP_inicio/admin/index.php
+++ 11-Bienes-Raices/bienesRaicesPHP_inicio/admin/index.php
@@ -1,11 +1,21 @@
 <?php
 
+// Importar la conexión
+require '../includes/funciones.php';
+$db = conectarDB();
+
+// Escribir el query
+$query = "SELECT * FROM propiedades";
+
+// Consultar la BD
+$resultadoConsulta = mysqli_query($db, $query);
+
+
 // Muestra mensaje condicional
 // $resultado = isset($_GET['resultado']) ? (int)filter_var( $_GET['resultado'], FILTER_SANITIZE_NUMBER_INT)  : 0;
 $resultado = (int)filter_var( $_GET['resultado'], FILTER_SANITIZE_NUMBER_INT) ?? 0;
 var_dump($_GET);
 var_dump($resultado);
-require '../includes/funciones.php';
 incluirTemplate('header');
 ?>
 
@@ -28,16 +38,18 @@ incluirTemplate('header');
             </thead>
 
             <tbody>
+            <?php while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
                 <tr>
-                    <td>1</td>
-                    <td>Casa en la playa</td>
-                    <td><img src="/imagenes/37608246e4fe3296343cf6424ba0f89c.jpg" class="imagen-tabla" alt="Imagen del anuncio" title="Imagen del anuncio"> </td>
-                    <td>$120000</td>
+                    <td><?= $propiedad['id']; ?></td>
+                    <td><?= $propiedad['titulo']; ?></td>
+                    <td><img src="/imagenes/<?= $propiedad['imagen'] ?>" class="imagen-tabla" alt="Imagen de la <?= $propiedad['titulo']; ?>" title="Imagen de la <?= $propiedad['titulo']; ?>"> </td>
+                    <td>$<?= $propiedad['precio']; ?></td>
                     <td class="alinear-centro">
                         <a href="#" class="boton-rojo-block">Eliminar</a>
                         <a href="#" class="boton-verde-block">Actualizar</a>
                     </td>
                 </tr>
+            <?php endwhile; ?>
             </tbody>
         </table>
     </main>
