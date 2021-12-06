f8c015712a3faa6905626f1d95659aca60938899
Josemi Xeon <worldcode.info@gmail.com>
2021-12-06 09:31:48 +0100

    Cambiada codificación de la BD de Bienes Raices

--- 11-Bienes-Raices/bienesRaicesPHP_inicio/documentos/bienes-raices.sql
+++ 11-Bienes-Raices/bienesRaicesPHP_inicio/documentos/bienes-raices.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema bienes_raices
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `bienes_raices` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci ;
+CREATE SCHEMA IF NOT EXISTS `bienes_raices` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 USE `bienes_raices` ;
 
 -- -----------------------------------------------------
