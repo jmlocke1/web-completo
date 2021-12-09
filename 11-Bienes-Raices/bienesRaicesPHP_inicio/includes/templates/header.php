cc0c931baaad8cd73fc052574301edfa5f9f538c
josemi <worldcode.info@gmail.com>
2021-12-09 11:51:21 +0100

    Añadida una nueva respuesta a preguntas importantes

--- 11-Bienes-Raices/bienesRaicesPHP_inicio/includes/templates/header.php
+++ 11-Bienes-Raices/bienesRaicesPHP_inicio/includes/templates/header.php
@@ -33,7 +33,7 @@ $auth = $_SESSION['login'] ?? false;
                         <a href="/blog.php">Blog</a>
                         <a href="/contacto.php">Contacto</a>
                     <?php if($auth): ?>
-
+                        <a href="cerrar-sesion.php">Cerrar Sesión</a>
                     <?php endif; ?>
                     </nav>
                 </div>
