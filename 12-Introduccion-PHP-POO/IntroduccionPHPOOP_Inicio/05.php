7e105562dd2a40774304defc57e41dac455853bd
josemi <worldcode.info@gmail.com>
2021-12-10 13:45:33 +0100

    Interfaces en PHP

--- 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/05.php
+++ 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/05.php
@@ -1,8 +1,6 @@
 <?php 
 declare(strict_types = 1);
 
-use Transporte as GlobalTransporte;
-
 include 'includes/header.php';
 
 abstract class Transporte {
--- 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/06.php
+++ 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/06.php
@@ -1,4 +1,38 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types = 1);
 
+include 'includes/header.php';
+
+interface TransporteInterfaz {
+    public function getInfo() : string;
+    public function getRuedas() : int;
+}
+   
+class Transporte implements TransporteInterfaz {
+	public function __construct(protected int $ruedas, protected int $capacidad)
+	{
+		
+	}
+
+	public function getInfo() : string {
+		return "El transporte tiene $this->ruedas ruedas y una capacidad de $this->capacidad persona".($this->capacidad > 1 ? 's.' : '.');
+	}
+    public function getRuedas() : int {
+		return $this->ruedas;
+	}
+}
+
+class Bicicleta extends Transporte {
+	public function __construct(protected int $ruedas, protected int $capacidad)
+	{
+		$this->nombre = "La Bicicleta";
+	}
+	// public function getInfo(): string
+	// {
+	// 	return $this->nombre . parent::getInfo();
+	// }
+
+	
+}
 
 include 'includes/footer.php';
