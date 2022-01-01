27a81c1ed9919c826f66e8547fa319a986b05a5c
josemi <worldcode.info@gmail.com>
2022-01-01 21:24:38 +0100

    Iniciando con composer y mysqli

--- 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/09.php
+++ 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/09.php
@@ -1,4 +1,10 @@
 <?php include 'includes/header.php';
 
+$db = new mysqli('localhost', 'usprueba', 'usprueba', 'bienes_raices');
+$query = "SELECT titulo from propiedades";
+$resultado = $db->query($query);
+var_dump($resultado->fetch_assoc());
+
+
 
 include 'includes/footer.php';
