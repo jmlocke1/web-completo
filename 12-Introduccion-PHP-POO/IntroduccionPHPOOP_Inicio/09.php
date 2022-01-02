b7488fbb85767877827d3d15078f2fd8994f0a31
Josemi Xeon <worldcode.info@gmail.com>
2022-01-02 02:28:56 +0100

    Consultas normales y preparadas con mysqli y pdo

--- 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/09.php
+++ 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/09.php
@@ -1,9 +1,29 @@
 <?php include 'includes/header.php';
-
+// CONSULTAS PREPARADAS
+// Conectamos a la base de datos
 $db = new mysqli('localhost', 'usprueba', 'usprueba', 'bienes_raices');
+
+// Creamos el query
 $query = "SELECT titulo from propiedades";
-$resultado = $db->query($query);
-var_dump($resultado->fetch_assoc());
+
+// Lo preparamos
+$stmt = $db->prepare($query);
+
+// Lo ejecutamos
+$stmt->execute();
+
+// Creamos la variable
+$stmt->bind_result($titulo);
+
+// Asignamos el resultado
+
+// Imprimir el resultado
+echo "<pre>";
+while($stmt->fetch()){
+	var_dump($titulo);
+}
+echo "</pre>";
+
 
 
 
--- 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/10.php
+++ 12-Introduccion-PHP-POO/IntroduccionPHPOOP_Inicio/10.php
@@ -1,4 +1,21 @@
 <?php include 'includes/header.php';
+// Conectar a la BD con PDO
+$db = new PDO('mysql:host=localhost; dbname=bienes_raices', 'usprueba', 'usprueba');
 
+// Creamos el query
+$query = "SELECT titulo, imagen from propiedades";
+
+// Consultar la BD
+$stmt = $db->prepare($query);
+$stmt->execute();
+$resultado = $stmt->fetchAll( PDO::FETCH_ASSOC );
+
+echo "<pre>";
+foreach($resultado as $propiedad){
+	echo $propiedad['titulo'], "<br>";
+	echo $propiedad['imagen'], "<br>";
+}
+//var_dump($resultado);
+echo "</pre>";
 
 include 'includes/footer.php';
