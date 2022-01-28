8209249b6e1d89144e4202797a176f9c5037fcd1
Josemi Xeon <worldcode.info@gmail.com>
2022-01-28 17:12:55 +0100

    Creando el Routing y Controlador para Autenticación

--- 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/controllers/Logincontroller.php
+++ 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/controllers/Logincontroller.php
@@ -1,11 +1,39 @@
 <?php
 namespace Controllers;
 class LoginController {
-    public static function login(){
-        echo "Desde login";
+    public static function loginGet(){
+        echo "Desde loginGet";
+    }
+
+    public static function loginPost(){
+        echo "Desde loginPost";
     }
 
     public static function logout(){
         echo "Desde logout";
+    }
+
+    public static function olvideGet(){
+        echo "Desde olvideGet";
+    }
+
+    public static function olvidePost(){
+        echo "Desde olvidePost";
+    }
+
+    public static function recuperarGet(){
+        echo "Desde RecuperarGet";
+    }
+
+    public static function recuperarPost(){
+        echo "Desde RecuperarPost";
+    }
+
+    public static function crearGet(){
+        echo "Desde crearCuentaGet";
+    }
+
+    public static function crearPost(){
+        echo "Desde crearCuentaPost";
     }
 }
--- 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/public/index.php
+++ 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/public/index.php
@@ -8,10 +8,18 @@ use MVC\Router;
 $router = new Router();
 
 // Iniciar sesión
-$router->get('/', [LoginController::class, 'login']);
-$router->post('/', [LoginController::class, 'login']);
+$router->get('/', [LoginController::class, 'loginGet']);
+$router->post('/', [LoginController::class, 'loginPost']);
 $router->get('/logout', [LoginController::class, 'logout']);
 
+// Recuperar el Password
+$router->get('/olvide', [LoginController::class, 'olvideGet']);
+$router->post('/olvide', [LoginController::class, 'olvidePost']);
+$router->get('/recuperar', [LoginController::class, 'recuperarGet']);
+$router->post('/recuperar', [LoginController::class, 'recuperarPost']);
 
+// Crear Cuenta
+$router->get('/crear-cuenta', [LoginController::class, 'crearGet']);
+$router->post('/crear-cuenta', [LoginController::class, 'crearPost']);
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
