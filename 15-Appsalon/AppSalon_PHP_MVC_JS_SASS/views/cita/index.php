05e91545e792fdc8bde83974ee3008ebaa890122
josemi <worldcode.info@gmail.com>
2022-02-03 13:17:31 +0100

    Esqueleto básico de las citas

--- 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/views/cita/index.php
+++ 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/views/cita/index.php
@@ -1,4 +1,19 @@
 <?php ?>
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuación</p>
-p.descripcion-
+<p class="descripcion-pagina">Elige tus servicios y coloca tus datos</p>
+
+<div id="app">
+    <div id="paso-1" class="seccion">
+        <h2>Servicios</h2>
+        <p>Elige tus servicios a continuación</p>
+        <div id="servicios" class="listado-servicios"></div>
+    </div>
+    <div id="paso-2" class="seccion">
+        <h2>Tus Datos y Citas</h2>
+        <p>Coloca tus datos y fecha de tu cita</p>
+    </div>
+    <div id="paso-3" class="seccion">
+        <h2>Resumen</h2>
+        <p>Verifica que la información sea correcta</p>
+    </div>
+</div>
