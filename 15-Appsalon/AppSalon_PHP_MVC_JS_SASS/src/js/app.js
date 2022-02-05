527bff826743739413444c847f2c32d6f57aa322
Josemi Xeon <worldcode.info@gmail.com>
2022-02-05 15:03:37 +0100

    Cambiando la sección según el tab presionado. El tab se almacena en localstorage

--- 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/src/js/app.js
+++ 15-Appsalon/AppSalon_PHP_MVC_JS_SASS/src/js/app.js
@@ -1,4 +1,8 @@
-let paso = 1;
+let paso = localStorage.getItem('paso');
+if (!paso) {
+    paso = 1;
+    localStorage.setItem('paso', 1);
+}
 
 document.addEventListener('DOMContentLoaded', function () {
     iniciarApp();
@@ -6,18 +10,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function iniciarApp() {
     // Cambia la sección cuando se presionan los tabs
+    mostrarSeccion();
     tabs();
 }
 
-function mostrarSeccion(paso) {
-    console.log('Mostrando sección ', paso)
+function mostrarSeccion() {
+    // Ocultar la sección que tenga la clase de mostrar
+    const seccionAnterior = document.querySelector('.mostrar');
+    if (seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar');
+    }
+    
+    // Seleccionar la sección con el paso
+    const seccion = document.querySelector(`#paso-${paso}`);
+    seccion.classList.add('mostrar');
+
+    // Quita la clase de actual al tab anterior
+    const tabAnterior = document.querySelector('.actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+    
+    // Resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${paso}"]`);
+    tab.classList.add('actual');
 }
 function tabs() {
     const botones = document.querySelectorAll('.tabs button');
     botones.forEach(boton => { 
         boton.addEventListener('click', function (e) {
             paso = parseInt(e.target.dataset.paso);
-            mostrarSeccion(paso);
+            localStorage.setItem('paso', paso);
+            mostrarSeccion();
         });
     });
 }
