47ac5bf384e26b18500f6ad4329620240b12146f
Josemi Xeon <worldcode.info@gmail.com>
2022-06-28 23:48:08 +0200

    Arreglado pequeño fallo en links del index

--- 14-Bienes-Raices-MVC/bienesRaices_MVC_inicio/views/paginas/index.php
+++ 14-Bienes-Raices-MVC/bienesRaices_MVC_inicio/views/paginas/index.php
@@ -31,7 +31,7 @@
                     <img width="200" height="300" loading="lazy" src="build/img/blog1.jpg" alt="Texto entrada blog" title="Texto entrada blog">
                 </picture>
                 <div class="texto-entrada">
-                    <a href="entrada.php" alt="Redirige a la entrada de blog ampliada" title="Redirige a la entrada de blog ampliada">
+                    <a href="blog/entrada" alt="Redirige a la entrada de blog ampliada" title="Redirige a la entrada de blog ampliada">
                         <h4>Terraza en el techo de tu casa</h4>
                         <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span></p>
                         <p>Consejos para construir una terraza en el techo de tu casa con los mejores materiales y ahorrando dinero</p>
@@ -45,7 +45,7 @@
                     <img width="200" height="300" loading="lazy" src="build/img/blog2.jpg" alt="Texto entrada blog" title="Texto entrada blog">
                 </picture>
                 <div class="texto-entrada">
-                    <a href="entrada.php" alt="Redirige a la entrada de blog ampliada" title="Redirige a la entrada de blog ampliada">
+                    <a href="blog/entrada" alt="Redirige a la entrada de blog ampliada" title="Redirige a la entrada de blog ampliada">
                         <h4>Guía para la decoración de tu hogar.</h4>
                         <p class="informacion-meta">Escrito el: <span>20/10/2021</span> por: <span>Admin</span></p>
                         <p>Maximiza el espacio en tu hogar con esta guía, aprende a combinar muebles y colores para darle vida a tu espacio.</p>
