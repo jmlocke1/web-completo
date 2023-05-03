e571698578a56109d9c4424e12ba167c8b8373a4
Josemi Xeon <worldcode.info@gmail.com>
2023-05-03 16:28:02 +0200

    Formulario de registro de ponentes

--- 17-DevWebCamp/DevWebCamp/views/admin/ponentes/crear.php
+++ 17-DevWebCamp/DevWebCamp/views/admin/ponentes/crear.php
@@ -6,3 +6,11 @@
 		Volver
 	</a>
 </div>
+
+<div class="dashboard__formulario">
+	<?php require_once __DIR__ . '/../../templates/alertas.php' ?>
+	<form method="POST" action="/admin/ponentes/crear" class="formulario">
+		<?php include_once __DIR__ . '/formulario.php';?>
+		<input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Ponente">
+	</form>
+</div>
---
+++ 17-DevWebCamp/DevWebCamp/views/admin/ponentes/formulario.php
@@ -0,0 +1,162 @@
+<fieldset class="formulario__fieldset">
+	<legend class="formulario__legend">Información Personal</legend>
+
+	<div class="formulario__campo">
+		<label for="nombre" class="formulario__label">Nombre</label>
+		<input 
+			type="text" 
+			name="nombre" 
+			id="nombre" 
+			class="formulario__input" 
+			placeholder="Nombre Ponente"
+			value="<?= $ponente->nombre ?? ''; ?>"
+		>
+	</div>
+	<div class="formulario__campo">
+		<label for="apellido" class="formulario__label">Apellidos</label>
+		<input 
+			type="text" 
+			name="apellido" 
+			id="apellido" 
+			class="formulario__input" 
+			placeholder="Apellidos del Ponente"
+			value="<?= $ponente->apellido ?? ''; ?>"
+		>
+	</div>
+	<div class="formulario__campo">
+		<label for="ciudad" class="formulario__label">Ciudad</label>
+		<input 
+			type="text" 
+			name="ciudad" 
+			id="ciudad" 
+			class="formulario__input" 
+			placeholder="Ciudad del Ponente"
+			value="<?= $ponente->ciudad ?? ''; ?>"
+		>
+	</div>
+	<div class="formulario__campo">
+		<label for="pais" class="formulario__label">País</label>
+		<input 
+			type="text" 
+			name="pais" 
+			id="pais" 
+			class="formulario__input" 
+			placeholder="País del Ponente"
+			value="<?= $ponente->pais ?? ''; ?>"
+		>
+	</div>
+	<div class="formulario__campo">
+		<label for="imagen" class="formulario__label">Imagen</label>
+		<input 
+			type="file" 
+			name="imagen" 
+			id="imagen" 
+			class="formulario__input formulario__input--file"
+		>
+	</div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+	<legend class="formulario__legend">Información Extra</legend>
+
+	<div class="formulario__campo">
+		<label for="tags_input" class="formulario__label">Áreas de experiencia (separadas por comas)</label>
+		<input 
+			type="text" 
+			id="tags_input" 
+			class="formulario__input" 
+			placeholder="Ej. Node.js, PHP, CSS, Laravel, UX / UI"
+		>
+		<div id="tags" class="formulario__listado"></div>
+		<input type="hidden" name="tags" value=""><?= $ponente->tags ?? ''; ?>
+	</div>
+</fieldset>
+
+<fieldset class="formulario__fieldset">
+	<legend class="formulario__legend">Redes Sociales</legend>
+
+	<div class="formulario__campo">
+		<div class="formulario__contenedor-icono">
+			<div class="formulario__icono">
+				<i class="fa-brands fa-facebook"></i>
+			</div>
+			<input 
+				type="text" 
+				name="redes[facebook]"
+				class="formulario__input--sociales" 
+				placeholder="Facebook"
+				value="<?= $ponente->facebook ?? ''; ?>"
+			>
+		</div>
+	</div>
+	<div class="formulario__campo">
+		<div class="formulario__contenedor-icono">
+			<div class="formulario__icono">
+				<i class="fa-brands fa-twitter"></i>
+			</div>
+			<input 
+				type="text" 
+				name="redes[twitter]"
+				class="formulario__input--sociales" 
+				placeholder="Twitter"
+				value="<?= $ponente->twitter ?? ''; ?>"
+			>
+		</div>
+	</div>
+	<div class="formulario__campo">
+		<div class="formulario__contenedor-icono">
+			<div class="formulario__icono">
+				<i class="fa-brands fa-youtube"></i>
+			</div>
+			<input 
+				type="text" 
+				name="redes[youtube]"
+				class="formulario__input--sociales" 
+				placeholder="YouTube"
+				value="<?= $ponente->youtube ?? ''; ?>"
+			>
+		</div>
+	</div>
+	<div class="formulario__campo">
+		<div class="formulario__contenedor-icono">
+			<div class="formulario__icono">
+				<i class="fa-brands fa-instagram"></i>
+			</div>
+			<input 
+				type="text" 
+				name="redes[instagram]"
+				class="formulario__input--sociales" 
+				placeholder="Instagram"
+				value="<?= $ponente->instagram ?? ''; ?>"
+			>
+		</div>
+	</div>
+	<div class="formulario__campo">
+		<div class="formulario__contenedor-icono">
+			<div class="formulario__icono">
+				<i class="fa-brands fa-tiktok"></i>
+			</div>
+			<input 
+				type="text" 
+				name="redes[tiktok]"
+				class="formulario__input--sociales" 
+				placeholder="Tiktok"
+				value="<?= $ponente->tiktok ?? ''; ?>"
+			>
+		</div>
+	</div>
+	<div class="formulario__campo">
+		<div class="formulario__contenedor-icono">
+			<div class="formulario__icono">
+				<i class="fa-brands fa-github"></i>
+			</div>
+			<input 
+				type="text" 
+				name="redes[github]"
+				class="formulario__input--sociales" 
+				placeholder="GitHub"
+				value="<?= $ponente->github ?? ''; ?>"
+			>
+		</div>
+	</div>
+</fieldset>
