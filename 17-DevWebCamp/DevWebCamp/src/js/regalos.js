e67ace14e0b3ffb755a1a405c45174da48a08fc8
Josemi Xeon <worldcode.info@gmail.com>
2023-08-09 00:52:42 +0200

    Comenzando con la gráfica de regalos

---
+++ 17-DevWebCamp/DevWebCamp/src/js/regalos.js
@@ -0,0 +1,71 @@
+(function() {
+	
+	// const ctx = document.getElementById('myChart');
+
+	// new Chart(ctx, {
+	// 	type: 'bar',
+	// 	data: {
+	// 		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	// 		datasets: [{
+	// 			label: '# of Votes',
+	// 			data: [12, 19, 3, 5, 2, 3],
+	// 			borderWidth: 1
+	// 		}]
+	// 	},
+	// 	options: {
+	// 		scales: {
+	// 			y: {
+	// 				beginAtZero: true
+	// 			}
+	// 		}
+	// 	}
+	// });
+
+	const grafica = document.querySelector('#regalos-grafica');
+    if(grafica) {
+            obtenerDatos()
+            async function obtenerDatos() {
+                const url = '/api/regalos'
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+
+                const ctx = document.getElementById('regalos-grafica').getContext('2d');
+                const myChart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: resultado.map( regalo => regalo.nombre),
+                        datasets: [{
+                            label: '',
+                            data: resultado.map( regalo => regalo.total),
+                            backgroundColor: [
+                                '#ea580c',
+                                '#84cc16',
+                                '#22d3ee',
+                                '#a855f7',
+                                '#ef4444',
+                                '#14b8a6',
+                                '#db2777',
+                                '#e11d48',
+                                '#7e22ce'
+                            ]
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        },
+                        plugins: {
+                            legend: {
+                                display: false
+                            }
+                        }
+                    }
+                });
+            }
+
+            
+    }
+  
+})();
--- 17-DevWebCamp/DevWebCamp/views/admin/regalos/index.php
+++ 17-DevWebCamp/DevWebCamp/views/admin/regalos/index.php
@@ -1,1 +1,5 @@
 <h2 class="dashboard__heading"><?= $titulo; ?></h2>
+
+<div>
+  <canvas id="myChart" width="400" height="400"></canvas>
+</div>
